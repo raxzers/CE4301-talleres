73d66dad63de43e44f5a72806f2894449fa82945
raxzers <gacova95@gmail.com>
2024-02-16 17:59:51 -0600

    Update taller.s 16/2/2024-1800h utc-6
    ya funciona de acuerdo a la especificación

--- taller1/taller.s
+++ taller1/taller.s
@@ -1,11 +1,11 @@
 start:
-    li a1,0x56         # [a1] <- 0x56 : lsfr_valor 
-    li a2,0x100         # [a2] <- 0x100 : mem_pos
-    sw a1,0(a2)         # 0x56 -> M[0x100] 
-    li a3,0x0           # [a3] <- 0x0 : counter
-    li a4,0x64          # [a4] <- 0x64 : stop_count(100)
+    li a1,0x56         # valor_semilla 0x56 en el registro a1
+    li a2,0x100        # mem_pos 0x100 en el registro a2
+    sw a1,0(a2)        # se guarda la semilla en memoria usando a2 
+    li a3,0x0          # contador en 0 en el registro a3
+    li a4,0x64         # numero maximo 0x64=100 en el registro a4
 lsfr:
-    li a0,0x80        #mascara al bit8
+    li a0,0x80         #mascara al bit8
     and t1,a0,a1
     srli t0,t1,0x7      # [t0] <- [lsfr_value] >> 2 (pos 8)
     
@@ -14,26 +14,27 @@ lsfr:
     srli t1,t1,0x5      # [t1] <- [lsfr_value] >> 3 (pos 5)
     xor t2,t1,t0        # [t2] <- [t1] ^ [t0] : [b6] xor [b5]
     
-     li a0,0x10
-     and t1,a0,a1
-     srli t1,t1,0x4
-     xor t2,t1,t2
-     
-     li a0,0x04
-     and t1,a0,a1
-     srli t1,t1,0x3
-     xor t2,t1,t2
-     slli t2,t2,0x7
-     srli a1,a1,0x1
-     add a1,t2,a1
+    li a0,0x10     
+    and t1,a0,a1
+    srli t1,t1,0x4
+    xor t2,t1,t2
+    
+    li a0,0x04
+    and t1,a0,a1
+    srli t1,t1,0x3
+    xor t2,t1,t2
+    
+    slli a1,a1,0x19
+    srli a1,a1,0x18
+    add a1,t2,a1
 
 check:
-    addi a3,a3,0x1      # counter = counter + 1
-    addi a2,a2,0x4      # mem_pos = mem_pos + 4
-    sw a1,0(a2)         # [lsfr_value] -> M[mem_pos]
-    blt a3,a4,lsfr      # counter < stop_count? -> lsfr
-    j end               # else: go to end
+    addi a3,a3,0x1      # contador+1
+    addi a2,a2,0x4      # mem_pos + 4
+    sw a1,0(a2)         # se guarda el nuevo valor en memoria usando mem_pos
+    blt a3,a4,lsfr      # if (contador < contadormaximo): salte a lsfr
+    j end               # else: salte a end
 
 
 end:
-    nop                  # end algorithm
+    nop                  # fin de algoritmo
